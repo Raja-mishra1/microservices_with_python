5b6295a591568fa22f379c87ef579e449c697a90
Mishra-raja <rajamishra0702@gmail.com>
2020-12-30 23:57:14 +0530

    fixed consumer.py bug

--- admin/consumer.py
+++ admin/consumer.py
@@ -5,29 +5,28 @@ django.setup()
 
 from products.models import Product
 
-params = pika.URLParameters('replace this with rabbit MQ url')
+params = pika.URLParameters("your_rabbitmq_url")
 
 connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='admin')
+channel.queue_declare(queue="admin")
+
 
 def callback(ch, method, properties, body):
-    print("Recieved in admin")
-    print(body)
-    # print('Received in admin')
-    # id = json.loads(body)
-    # print(id)
-    # product = Product.objects.get(id=id)
-    # product.likes = product.likes + 1
-    # product.save()
-    # print('Product likes increased!')
+    print("Received in admin")
+    id = json.loads(body)
+    print(id)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print("Product likes increased!")
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback,auto_ack=True)
+channel.basic_consume(queue="admin", on_message_callback=callback, auto_ack=True)
 
-print('Started Consuming')
+print("Started Consuming")
 
 channel.start_consuming()
 
